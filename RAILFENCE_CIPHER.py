aa0d10ec5eff859dd81b4f8c00a70552e8ff917c
SankalpaHettiarachchi <itt1819041@tec.rjt.ac.lk>
2023-12-29 01:20:57 +0530

    Rail fence ciper done

--- RAILFENCE_CIPHER.py
+++ RAILFENCE_CIPHER.py
@@ -36,7 +36,71 @@ class RailFence_Encryption:
         
         print(ciphertext)
            
+class RailFence_Decryption:
+    def __init__(self, key,ciphertext):
+        length = len(ciphertext)
+        plaintext = "." * length
+        cycle = 2 * key - 2
+        units = length // cycle
+        rail_lengths = [0] * key
+        
+        # Top Rail Length
+        rail_lengths[0] = units
+        
+        # Intermidiate Rail length
+        for i in range(1, key - 1):
+            rail_lengths[i] = 2 * units
+            
+        # Bottom Rail length
+        rail_lengths[key - 1] = units
+        
+        for i in range(length % cycle):
+            if i < key:
+                rail_lengths[i] += 1
+            
+            else:
+                rail_lengths[cycle - i] += 1
+        
+        print(rail_lengths)
+        
+        # Replace characters in the top rail
+        index = 0
+        rail_offset = 0
+        for c in ciphertext[:rail_lengths[0]]:
+            plaintext = plaintext[:index] + c + plaintext[index + 1 :]
+            index+=cycle
+        rail_offset += rail_lengths[0]
+        print(plaintext)
+        
+        # Replace characters in the intermidiate rail
+        for row in range(1, key - 1):
+            left_index = row
+            right_index = cycle - row
+            left_char = True
+            
+            for c in ciphertext[rail_offset:rail_offset + rail_lengths[row]]:
+                if left_char:
+                    plaintext = plaintext[:left_index] + c + plaintext[left_index+1:]
+                    left_index+=cycle
+                    left_char = not left_char
+                
+                else:
+                    plaintext = plaintext[:right_index] + c + plaintext[right_index+1:]
+                    right_index += cycle
+                    left_char = not left_char
+            
+            rail_offset += rail_lengths[row]
+            print(plaintext)
+            
+        
+        # Replace characters in the last rail
+        index = key - 1
+        for c in ciphertext[rail_offset:]:
+            plaintext = plaintext[:index] + c + plaintext[index + 1 :]
+            index+=cycle
 
+        print(plaintext)
+        
 def main():
     while True:
         print("====================================================================")
@@ -47,16 +111,16 @@ def main():
             plaintext = input("Enter the text to encrypt: ")
             ciphertext = RailFence_Encryption(key,plaintext)            
             
-        # elif choice == 'D':                                   
-        #     ciphertext = input("Enter the text to decrypt: ")
-        #     plaintext = RailFence_Decryption(key,ciphertext)           
+        elif choice == 'D':                                   
+            ciphertext = input("Enter the text to decrypt: ")
+            plaintext = RailFence_Decryption(key,ciphertext)           
             
-        # else:
-        #     print("Invalid choice. Please enter 'e' to encrypt or 'd' to decrypt.")
+        else:
+            print("Invalid choice. Please enter 'e' to encrypt or 'd' to decrypt.")
             
 if __name__ == "__main__":
     main()       
                                                
     # this is top secret
     # 4
-    # 
+    # tsshi  eti tpcesor
